2f29839a800454bec864988265d5888cd114a45e
taehopark0! <taehojj@gmail.com>
2020-12-06 23:51:37 +0900

    Naver Shopping Scrapping from 1pg to 8pg
    List of Name/Price/Product type

--- 201206_scrapping.py
+++ 201206_scrapping.py
@@ -1,3 +1,4 @@
+
 import requests
 import time
 from bs4 import BeautifulSoup
@@ -8,42 +9,60 @@ from selenium.webdriver.common.keys import Keys
 import pandas as pd
 
 ua = UserAgent()
-URL = []
-url = "https://search.shopping.naver.com/search/all?query=생리대&cat_id=&frm=NVSHAKW"
+URL = "https://search.shopping.naver.com/search/all?frm=NVSHAKW&origQuery=생리대&pagingIndex=1&pagingSize=40&productSet=total&query=생리대&sort=rel&timestamp=&viewType=list"
 headers = {'User-Agent':ua.random}
 headline_list =[]
-
-driver = webdriver.Chrome("/Users/taehopark/PycharmProjects/dalchaebi/Naver Crawling/Naver Shopping/chromedriver")
-driver.get("https://search.shopping.naver.com/search/all?query=생리대&cat_id=&frm=NVSHAKW")
-
-SCROLL_PAUSE_TIME = 0.5
-
-# Get scroll height
-last_height = driver.execute_script("return document.body.scrollHeight")
-
-while True:
-    # Scroll down to bottom
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-    # Wait to load page
-    time.sleep(SCROLL_PAUSE_TIME)
-
-    # Calculate new scroll height and compare with last scroll height
-    new_height = driver.execute_script("return document.body.scrollHeight")
-    if new_height == last_height:
-        break
-    last_height = new_height
 title_list=[]
 price_list=[]
 type_list=[]
-boxes = driver.find_elements_by_css_selector(".basicList_info_area__17Xyo")
-for i in range(len(boxes)):
-    title_list.append(boxes[i].find_element_by_css_selector(".basicList_link__1MaTN").get_attribute("title"))
-    price_list.append(boxes[i].find_element_by_css_selector(".price_num__2WUXn").text)
-    type_list.append(boxes[i].find_element_by_css_selector(".basicList_desc__2-tko").text)
+
+driver = webdriver.Chrome("/Users/taehopark/PycharmProjects/dalchaebi/Naver Crawling/Naver Shopping/chromedriver")
+
+
+URL1=requests.get(URL)
+URL2=BeautifulSoup(URL1.text,'html.parser')
+
+pages_number = URL2.find_all('a',{'class':'pagination_btn_page__FuJaU'})
+
+product_link_list = []
+split = URL.split('1')
+for i in range(len(pages_number)):
+    product_link_list.append(split[0] + str(i) + '&pagingSize=40&productSet=total&query=생리대&sort=rel&timestamp=&viewType=list')
+assert len(product_link_list)!=0, "product list is empty"
+
+print(len(product_link_list))
+
+n=1
+for i in range(len(product_link_list)):
+    driver.get(product_link_list[i])
+
+    SCROLL_PAUSE_TIME = 0.5
+
+# Get scroll height
+    last_height = driver.execute_script("return document.body.scrollHeight")
+
+
+    while True:
+    # Scroll down to bottom
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    # Wait to load page
+        time.sleep(SCROLL_PAUSE_TIME)
+
+    # Calculate new scroll height and compare with last scroll height
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            break
+        last_height = new_height
+
+    boxes = driver.find_elements_by_css_selector(".basicList_info_area__17Xyo")
+    for j in range(len(boxes)):
+        title_list.append(boxes[j].find_element_by_css_selector(".basicList_link__1MaTN").get_attribute("title"))
+        price_list.append(boxes[j].find_element_by_css_selector(".price_num__2WUXn").text)
+        type_list.append(boxes[j].find_element_by_css_selector(".basicList_desc__2-tko").text)
+
+    driver.find_element_by_css_selector(".pagination_btn_page__FuJaU").click()
+    n+=1
 
 df = pd.DataFrame({"title": title_list, "price": price_list, "type&size":type_list})
-df.to_excel("navershopping_20201129.xlsx", index=False)
-
-
-
+df.to_excel("result_scrapping_navershopping.xlsx", index=False)
